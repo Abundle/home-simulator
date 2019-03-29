d7f5ea5e9261d93e101fff3b3957674abae63c92
Abundle <34342677+Abundle@users.noreply.github.com>
2019-03-29 19:45:40 +0100

    Initiate 3D canvas 🚀

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './scss/main.scss';
+import Scene  from './Scene';
 
 import { MDCRipple } from '@material/ripple/index';
 
 const ripple = new MDCRipple(document.querySelector('.mdc-button'));
+
+Scene();
