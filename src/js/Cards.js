f37be2766afccaf29ce116983a52e0abd665b4e0
Abundle <34342677+Abundle@users.noreply.github.com>
2019-06-10 13:24:18 +0200

    Add category titles 📰

--- src/js/Cards.js
+++ src/js/Cards.js
@@ -1,33 +1,44 @@
 import { items } from './items.js';
 
-export let Cards = /*language=HTML*/`
-    ${ items.map((item) => `
-         <div class='mdc-card demo-card demo-basic-with-text-over-media'>
-            <div class='mdc-card__primary-action demo-card__primary-action' tabindex='0'> 
-                <div class='mdc-card__media mdc-card__media--16-9 demo-card__media' style='background-image: url(${ require('../assets/img/' + item.image) });'>
-                    <div class='mdc-card__media-content'>
-                        <div class='mdc-card__primary'>
-                            <h2 class='mdc-card__title mdc-typography mdc-typography--headline6'>${ item.title }</h2>
-                            <h3 class='mdc-card__subtitle mdc-typography mdc-typography--subtitle2'>${ item.subtitle }</h3>
+export const Cards = Object.keys(items).map(category => `
+        <h1 class='mdc-typography--headline5'>${ category }</h1>
+        ${ items[category].map(item => `
+            <div class='mdc-card demo-card demo-basic-with-text-over-media'>
+                <div class='mdc-card__primary-action demo-card__primary-action' tabindex='0'>
+                    <div class='mdc-card__media mdc-card__media--16-9 demo-card__media' style='background-image: url(${ require('../assets/img/' + item.image) });'>
+                        <div class='mdc-card__media-content'>
+                            <div class='mdc-card__primary'>
+                                <h2 class='mdc-card__title mdc-typography mdc-typography--headline6'>${ item.title }</h2>
+                                <h3 class='mdc-card__subtitle mdc-typography mdc-typography--subtitle2'>${ item.subtitle }</h3>
+                            </div>
                         </div>
                     </div>
+                    <div class='mdc-card__secondary mdc-typography mdc-typography--body2'>Visit ten places on our planet that are undergoing the biggest changes today.</div>
                 </div>
-                <div class='mdc-card__secondary mdc-typography mdc-typography--body2'>Visit ten places on our planet that are undergoing the biggest changes today.</div>
+                <div class='mdc-card__actions'>
+                    <div class='mdc-card__action-buttons'>
+                        <button class='mdc-button mdc-card__action mdc-card__action--button'>Read</button>
+                        <button class='mdc-button mdc-card__action mdc-card__action--button'>Bookmark</button>
+                    </div>
+                    <div class='mdc-card__action-icons'>
+                        <button class='mdc-icon-button mdc-card__action mdc-card__action--icon--unbounded' aria-pressed='false' aria-label='Add to favorites' title='Add to favorites'>
+                            <i class='material-icons mdc-icon-button__icon mdc-icon-button__icon--on'>favorite</i>
+                            <i class='material-icons mdc-icon-button__icon'>favorite_border</i>
+                        </button>
+                        <button class='mdc-icon-button material-icons mdc-card__action mdc-card__action--icon--unbounded' title='Share' data-mdc-ripple-is-unbounded='true'>share</button>
+                        <button class='mdc-icon-button material-icons mdc-card__action mdc-card__action--icon--unbounded' title='More options' data-mdc-ripple-is-unbounded='true'>more_vert</button>
+                    </div>
+                </div>
             </div>
-            <div class='mdc-card__actions'>
-                <div class='mdc-card__action-buttons'>
-                    <button class='mdc-button mdc-card__action mdc-card__action--button'>Read</button>
-                    <button class='mdc-button mdc-card__action mdc-card__action--button'>Bookmark</button>
-                </div>
-                <div class='mdc-card__action-icons'>
-                    <button class='mdc-icon-button mdc-card__action mdc-card__action--icon--unbounded' aria-pressed='false' aria-label='Add to favorites' title='Add to favorites'>
-                        <i class='material-icons mdc-icon-button__icon mdc-icon-button__icon--on'>favorite</i>
-                        <i class='material-icons mdc-icon-button__icon'>favorite_border</i>
-                    </button>
-                    <button class='mdc-icon-button material-icons mdc-card__action mdc-card__action--icon--unbounded' title='Share' data-mdc-ripple-is-unbounded='true'>share</button>
-                    <button class='mdc-icon-button material-icons mdc-card__action mdc-card__action--icon--unbounded' title='More options' data-mdc-ripple-is-unbounded='true'>more_vert</button>
-                </div>
-            </div>
-        </div>
-    `).join('\n ') }
-`;
+        `).join('') }
+    `).join('');
+
+/*export const Cards = Object.keys(items).map(category => `
+        <li>${ category }
+            ${ items[category].map(item => `
+                <p>
+                    ${ item }
+                </p>
+            `).join('') }
+        </li>
+    `).join('');*/
--- src/js/items.js
+++ src/js/items.js
@@ -1,4 +1,50 @@
-export const items = [
+
+export const items = {
+    'Default': [
+        {
+            id        : 1,
+            category  : 'default',
+            title     : 'Our Changing Planet',
+            subtitle  : 'by Kurt Wagner',
+            content   : 'Visit ten places on our planet that are undergoing the biggest changes today.',
+            image     : 'placeholder.jpg',
+        },
+        {
+            id        : 2,
+            category  : 'default',
+            title     : 'Lorem ipsum',
+            subtitle  : 'by Hello World',
+            content   : 'Lorem ipsum.',
+            image     : 'placeholder.jpg'
+        }
+    ],
+    'Kitchen': [
+        {
+            id: 3,
+            category: 'default',
+            title: 'Lorem ipsum',
+            subtitle: 'by Hello World',
+            content: 'Lorem ipsum.',
+            image: 'placeholder.jpg'
+        },
+        {
+            id: 4,
+            category: 'default',
+            title: 'Lorem ipsum',
+            subtitle: 'by Hello World',
+            content: 'Lorem ipsum.',
+            image: 'placeholder.jpg'
+        }]
+};
+
+// https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg
+
+/*export const items = {
+    'Default': ['apple', 'pear'],
+    'Kitchen': ['lemon', 'banana']
+};*/
+
+/*export const items = [
     {
         id        : 1,
         category  : 'default',
@@ -32,5 +78,4 @@ export const items = [
         image     : 'placeholder.jpg'
     }
 ];
-
-// https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg
+*/
